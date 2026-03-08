ff2692691f2f3c9722fc54daa214d44b06b0203d
Kadir0642 <kadirk061942@gmail.com>
2026-03-08 11:56:51 +0300

    feat: findByUserId query(Bring the wardrobe belonging to ID) added

--- src/main/java/com/MyWardrobe/backend/repository/ClothingItemRepository.java
+++ src/main/java/com/MyWardrobe/backend/repository/ClothingItemRepository.java
@@ -3,10 +3,13 @@ package com.MyWardrobe.backend.repository;
 import com.MyWardrobe.backend.entity.ClothingItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository // Database elemanı
 public interface ClothingItemRepository extends JpaRepository<ClothingItem, Long> {
 
+    // Bu ID ye sahip kullanıcının dolabındaki tüm parçaları getir
+    List<ClothingItem> findByUserId(Long userId); // AI ve Uygulama için sorgu
     // JpaRepository kütüphanesi sayesinde SQL yazmadan .save(), .findALl(), . deleteById() komutlarla veritabanını yönetebiliriz.
     // İleride buraya AI algoritması için "Bu kullanıcının kışlık eşyalarını getir" tarzı komutlar yazacağız.
 }
