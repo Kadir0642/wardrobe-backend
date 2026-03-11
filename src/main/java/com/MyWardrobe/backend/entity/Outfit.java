128a6a9f33b222a7d81bca08cf955e06150325e4
Kadir0642 <kadirk061942@gmail.com>
2026-03-11 14:41:13 +0300

    feat: add Outfit entity with ManyToMany relationship to ClothingItem

---
+++ src/main/java/com/MyWardrobe/backend/entity/Outfit.java
@@ -0,0 +1,45 @@
+package com.MyWardrobe.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity@Table(name="outfits")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Outfit {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name; // "Kişlık Ofis Kombini", "Spor Salonu"
+
+
+    // 1.İLİŞKİ: Bu kombin kime ait?
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+
+    // 2.İLİŞKİ: Bu kombinin içinde hangi kıyafetler var? (En önemli yer)
+    @ManyToMany  //  Çoka Çok (N:M)" ilişkisi için araya üçüncü bir "Köprü Tablo" (Junction Table)
+    @JoinTable( // Spring Boot ayağa kalkarken outfit_clothing_items adında üçüncü, görünmez bir tablo oluşacak.
+            //Bu tablo sadece-> 3 numaralı kombin, 5 ve 8 numaralı kıyafetlerden oluşur." Bu sayede siyah tişörtünü istediğin kadar farklı kombine ekleyebileceksin!
+            name="outfit_clothing_items", // Spring Boot bu isimde gizli bir köprü tablosu kuracak
+            joinColumns = @JoinColumn(name= "outfit_id"),
+            inverseJoinColumns= @JoinColumn(name="clothing_item_id")
+    )
+    private List<ClothingItem> clothingItems;
+
+    private LocalDateTime createdAt; // Kıyafetin ne zaman oluşturulduğunu bize söyler
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt=LocalDateTime.now();
+    }
+
+}
